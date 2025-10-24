4d6470148ef9752f0d9929227d116f921b05efa8
EXTRACTOR-QR1 <gauravdlp2025@gmail.com>
2025-10-24 21:41:36 +0530

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,12 @@
+# Don't Remove Credit Tg - @ALL_CORSE_H
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+from os import environ
+
+API_ID = int(environ.get("API_ID", "16902680"))
+API_HASH = environ.get("API_HASH", "f1c763293059f278918c3e8d15e8f9b2")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8282357869:AAF0qenHKzHhIT-gKxWRem3RV-P_F3xvJB0")
+
+
+
